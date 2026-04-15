f51e0e552a366bd88881d2f7dad117413054d837
Mitchepv <143571339+Mitchepv@users.noreply.github.com>
2026-04-15 16:03:38 -0400

    Initial Commit

---
+++ Dance Me/Dance_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dance_MeApp.swift
+//  Dance Me
+//
+//  Created by Nia Mitchell on 4/15/26.
+//
+
+import SwiftUI
+
+@main
+struct Dance_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
